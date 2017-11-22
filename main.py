3c32be03753989cded611c74976c4ab87515e8c3
LookACastle <thorkik@gmail.com>
2017-11-22 11:24:42 +0100

    window is constant and as such should be WINDOW

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import pygame, sys
 from constants import *
 
 pygame.init()
-window = pygame.display.set_mode((1280,720))
+WINDOW = pygame.display.set_mode((1280,720))
 
 #Input handling
 def keyboard(event):
-	global window
+	global WINDOW
 	if event.key == pygame.K_w: #test
-		window.fill(WHITE)
+		WINDOW.fill(WHITE)
 	if event.key == pygame.K_p: #test
-		window.fill(PASTEL_PINK)
+		WINDOW.fill(PASTEL_PINK)
 	if event.key == pygame.K_ESCAPE:
 		pygame.quit()
 		sys.exit()
