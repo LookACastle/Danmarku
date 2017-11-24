b42d1685b99199e3692aed4ad62925c4663d75e8
emildichmann272 <Emil@dichmann.com>
2017-11-24 13:18:40 +0100

    Added functions for types of bullet patterns needed to be implemented

---
+++ bullets.py
@@ -0,0 +1,15 @@
+from main import *
+
+def BulletShower(x, amount, spread, sprite):
+	
+
+def BulletFan(x, amount, spread, delay, sprite):
+	
+
+def StraightDown(x,y, Tracer, sprite):
+	
+
+def Function(funcx, funcy, x, y, sprite):
+	
+
+
