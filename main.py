9af17cdfb42a639cc6fe6e18c11e715035f2a63c
LookACastle <thorkik@gmail.com>
2017-11-24 13:06:23 +0100

    Actually fixed keyboard handling
    sorry for committing without testing like a fcuking moron

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from gfx.loadAllSprites import load_Sprites
 pygame.init()
 MONITOR_INFO = pygame.display.Info()
 WINDOW = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-keyboard_event = False
 
 loaded_Sprites = [[], [], []]
 
@@ -18,36 +17,31 @@ test2 = None
 #Input handling
 def keyboard(event):
 	global WINDOW
-	if event.key == pygame.K_w: #test
+	pressed = pygame.key.get_pressed()
+	if pressed[pygame.K_w]: #test
 		WINDOW.fill(WHITE)
-	if event.key == pygame.K_p: #test
+	if pressed[pygame.K_p]: #test
 		WINDOW.fill(PASTEL_PINK)
-	if event.key == pygame.K_x: #test
+	if pressed[pygame.K_x]: #test
 		global test, test2
 		test = Sprites(int(MONITOR_INFO.current_w/2), int(MONITOR_INFO.current_h/2), loaded_Sprites, PLAYERSPRITESARRAY, ON_PLANE)
 		test.render_Sprites(WINDOW)
 		test2 = Sprites(int(MONITOR_INFO.current_w/2) + 130, int(MONITOR_INFO.current_h/2) + 130, loaded_Sprites, ENEMYSPRITESARRAY, PURPLE_CIRCLE)
 		test2.render_Sprites(WINDOW)
 		#pygame.draw.circle(WINDOW,PASTEL_PINK,(int(MONITOR_INFO.current_w/2), int(MONITOR_INFO.current_h/2)), 100, 9)
-	if event.key == pygame.K_z:
+	if pressed[pygame.K_z]:
 		test2 = Sprites(int(MONITOR_INFO.current_w/2), int(MONITOR_INFO.current_h/2), loaded_Sprites, ENEMYSPRITESARRAY, PURPLE_CIRCLE)
 		test2.render_Sprites(WINDOW)
-	if event.key == pygame.K_ESCAPE:
+	if pressed[pygame.K_ESCAPE]:
 		pygame.quit()
 		sys.exit()
+	else:
+		pass
 
 
 
 while True:
-	if pygame.KEYDOWN:
-		keyboard_event = True
-	elif pygame.KEYUP:
-		keyboard_event = False
-		
-
-	for event in pygame.event.get():
-		if keyboard_event:
-			keyboard(event)
+	keyboard(pygame.event.get(pygame.KEYDOWN))
 	
 	if test is not None and test2 is not None:
 		if pygame.sprite.collide_mask(test, test2):
