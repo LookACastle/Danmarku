7ec3acc65034b7849187e926ba7d472e22735f60
LookACastle <thorkik@gmail.com>
2017-11-29 13:26:14 +0100

    Fixed ghosting
    Horrible hardcoding was also removed.

--- entities/player.py
+++ entities/player.py
@@ -10,28 +10,28 @@ class Player(pygame.sprite.Sprite):
         self.window = window
         self.player = self.array[PLAYERSPRITESARRAY][ON_PLANE].copy()
         pygame.sprite.Sprite.__init__(self)
-        self.rect = pygame.Rect(self.x-65, self.y-65, 130, 130)
         self.mask = pygame.mask.from_surface(self.player)
-        self.speed = 5
+        self.speed = 7.5
+        self.rect = pygame.Rect(self.x - 64 + self.speed, self.y - 64 + self.speed, 128 + self.speed*2, 128 + self.speed*2)
 		
     def movement(self, event, width, height):
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_w]:
             if (self.y >= 64):
                 self.y -= self.speed
-                self.rect = pygame.Rect(self.x-65, self.y-65, 130, 130)
+                self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
         if pressed[pygame.K_s]:
             if (self.y <= height - 64):
                 self.y += self.speed
-                self.rect = pygame.Rect(self.x-65, self.y-65, 130, 130)
+                self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
         if pressed[pygame.K_a]:
             if (self.x >= 64):
                 self.x -= self.speed
-                self.rect = pygame.Rect(self.x-65, self.y-65, 130, 130)
+                self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
         if pressed[pygame.K_d]:
             if (self.x <= width - 64):
                 self.x += self.speed
-                self.rect = pygame.Rect(self.x-65, self.y-65, 130, 130)
+                self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
     
     def render(self, window):
         window.blit(self.player, (self.x - 64, self.y - 64))
