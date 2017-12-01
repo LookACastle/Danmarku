18a6308e85bdb46483c8b2497f84d85cd61cc7da
zentarg <apg.chili@gmail.com>
2017-12-01 12:44:37 +0100

    Added player shooting
    |Added new player bullet sprite \n |Added new python file in entities for the projective \n |Removes and updates player bullets the same way as enemy bullets in main.py while loop \n |Added PLAYER_SPEED and PLAYER_PROJECTILE_SPEED to constants.py

--- entities/player.py
+++ entities/player.py
@@ -1,17 +1,19 @@
 import pygame
 from constants import *
 from gfx.loadSprite import Sprites
+from entities.player_projectile import Player_Projectile
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, array, window):
+    def __init__(self, x, y, array, window, objectsArray):
         self.x = x
         self.y = y
         self.array = array
         self.window = window
+        self.objectsArray = objectsArray
         self.player = self.array[PLAYERSPRITESARRAY][ON_PLANE].copy()
         pygame.sprite.Sprite.__init__(self)
         self.mask = pygame.mask.from_surface(self.player)
-        self.speed = 7.5
+        self.speed = PLAYER_SPEED
         self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
 		
     def movement(self, event, width, height):
@@ -32,6 +34,9 @@ class Player(pygame.sprite.Sprite):
             if (self.x <= width - 64):
                 self.x += self.speed
                 self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
+        if pressed[pygame.K_SPACE]:
+            bullet = Player_Projectile(self.x, self.y+PLAYER_PROJECTILE_SPEED, self.array, self.window)
+            self.objectsArray.append(bullet)
     def render(self, window):
         window.blit(self.player, (self.x - 64, self.y - 64))
 
--- main.py
+++ main.py
@@ -17,11 +17,12 @@ else:
 loaded_Sprites = [[], [], []]
 render_objects = []
 renderBullets = []
+renderPlayerBullets = []
 load_Sprites(loaded_Sprites)
 clock = pygame.time.Clock()
 window_w, window_h = pygame.display.get_surface().get_size()
 
-player = Player(int(window_w/2), int(window_h/2), loaded_Sprites, WINDOW)
+player = Player(int(window_w/2), int(window_h/2), loaded_Sprites, WINDOW, renderPlayerBullets)
 render_objects.append(player)
 playerHitbox = PlayerHitbox(loaded_Sprites, WINDOW, player)
 
@@ -48,19 +49,24 @@ while True:
 	clock.tick(MAX_FPS)
 	framecount+=1 
 	if (time.time()-start_time >= 1):
-		print(len(renderBullets))
+		print("Enemy Bullets: " + str(len(renderBullets)) + " | Player Bullets: " + str(len(renderPlayerBullets)))
 		print("FPS:"+ str(math.floor(framecount/(time.time()-start_time))))
 		framecount = 0
 		start_time = time.time()
 	keyboard(pygame.event.get(pygame.KEYDOWN))
 	player.movement(pygame.event.get(pygame.KEYDOWN), window_w, window_h)
 	playerHitbox.movement(pygame.event.get(pygame.KEYDOWN))
-	if player is not None and playerHitbox is not None:
+	'''if player is not None and playerHitbox is not None:
 		if pygame.sprite.collide_mask(player, playerHitbox):
 			print("COLLISION")
 		else:
-			print("no collision :(")
+			print("no collision :(")'''
 	WINDOW.fill((66, 194, 244))
+	for i in range((len(renderPlayerBullets)-1), -1, -1):
+		renderPlayerBullets[i].render(WINDOW)
+		remove = renderPlayerBullets[i].move(window_w, window_h)
+		if (remove):
+			renderPlayerBullets.pop(i)
 	for i in range((len(renderBullets)-1),-1,-1):
 		renderBullets[i].render(WINDOW)
 		remove = renderBullets[i].move(window_w, window_h)
@@ -69,5 +75,6 @@ while True:
 	render(render_objects, WINDOW)
 	playerHitbox.render(WINDOW)
 	pygame.display.update(render_objects)
+	pygame.display.update(renderPlayerBullets)
 	pygame.display.update(renderBullets)
 	pygame.display.update(playerHitbox)
