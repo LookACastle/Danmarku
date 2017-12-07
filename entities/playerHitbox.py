f78cbe307770cb5f8fc14f5c41add3a2baf2c8e9
zentarg <apg.chili@gmail.com>
2017-12-07 09:30:07 +0100

    Added pre-laser sprite, changed hitbox rect method

--- entities/playerHitbox.py
+++ entities/playerHitbox.py
@@ -14,15 +14,17 @@ class PlayerHitbox(pygame.sprite.Sprite):
         self.hitbox = self.array[PLAYERSPRITESARRAY][PLAYER_HITBOX].copy()
         pygame.sprite.Sprite.__init__(self)
         self.mask = pygame.mask.from_surface(self.hitbox)
-        self.speed = 7.5
-        self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
+        self.speed = PLAYER_SPEED
+        self.rect = self.hitbox.get_rect(center=(self.x, self.y),width=(20+self.speed*2),height=(20+self.speed*2))
+        #self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
 		
     def movement(self, event):
         pressed = pygame.key.get_pressed()
         self.position = self.player.getPos()
         self.x = self.position[0]
         self.y = self.position[1]
-        self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
+        self.rect = self.hitbox.get_rect(center=(self.x, self.y),width=(20+self.speed*2),height=(20+self.speed*2))
+        #self.rect = pygame.Rect(self.x - (64 + self.speed), self.y - (64 + self.speed), 128 + self.speed*2, 128 + self.speed*2)
         if pressed[pygame.K_LSHIFT]:
             self.t = True
         else:
